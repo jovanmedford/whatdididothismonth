f0738c411fce07b56921317bce0b2af5956bedb5
Jovan <jovanmedford@gmail.com>
2025-02-08 16:53:58 -0500

    Add get started button to banner, and log in / sign up text to banner

--- app/header.tsx
+++ app/header.tsx
@@ -1,4 +1,12 @@
+import Link from "next/link";
 
 export default function Header() {
-    return <p className="px-4 py-8">What Did I Do This Month?</p>
+  return (
+    <header className="flex justify-between px-4 py-8">
+      <p>What Did I Do This Month?</p>
+      <div className="flex gap-1">
+        <Link href="#">Log in</Link> / <Link href="#">Sign up</Link>
+      </div>
+    </header>
+  );
 }
--- app/main-banner.tsx
+++ app/main-banner.tsx
@@ -1,11 +1,14 @@
+import LinkButton from "./_components/button";
+
 export default function MainBanner() {
   return (
-    <section className="text-center bg-beige-500 py-20 px-2 text-deep-blue-500">
+    <section className="text-center bg-beige-500 py-16 px-2 text-deep-blue-500">
       <h1 className="mb-4 mt-0 text-4xl font-bold">Make The Days Count</h1>
-      <p>
+      <p className="mt-4 mb-6">
         Take control of every aspect of your life and then you will have an even
         better life.
       </p>
+      <LinkButton href="#">Get Started</LinkButton>
     </section>
   );
 }
