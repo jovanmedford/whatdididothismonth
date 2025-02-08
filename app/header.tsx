480d3e6645017db0f5aedf870e4320fbb2d8a363
Jovan <jovanmedford@gmail.com>
2025-02-08 16:15:07 -0500

    Add tailwind, remove boilerplate css, add a few themed styles and begin home page

---
+++ app/header.tsx
@@ -0,0 +1,4 @@
+
+export default function Header() {
+    return <p className="px-4 py-8">What Did I Do This Month?</p>
+}
---
+++ app/main-banner.tsx
@@ -0,0 +1,11 @@
+export default function MainBanner() {
+  return (
+    <section className="text-center bg-beige-500 py-20 px-2 text-deep-blue-500">
+      <h1 className="mb-4 mt-0 text-4xl font-bold">Make The Days Count</h1>
+      <p>
+        Take control of every aspect of your life and then you will have an even
+        better life.
+      </p>
+    </section>
+  );
+}
