29f52c5bdf5b69a76ff3a3c6fa73da91312cfdf9
Jovan <jovanmedford@gmail.com>
2025-02-16 11:58:31 -0500

    Add login and signup page links

--- app/header.tsx
+++ app/header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
     <header className="flex justify-between px-4 py-8">
       <p>What Did I Do This Month?</p>
       <div className="flex gap-1">
-        <Link href="#">Log in</Link> / <Link href="#">Sign up</Link>
+        <Link href="login">Log in</Link> / <Link href="sign-up">Sign up</Link>
       </div>
     </header>
   );
