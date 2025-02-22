c60f67feeeeecf68fc6f119d258f7339a6125da0
Jovan <jovanmedford@gmail.com>
2025-02-21 21:33:27 -0500

    Add basic app header

---
+++ app/app/app-header.tsx
@@ -0,0 +1,14 @@
+export default function AppHeader() {
+  return (
+    <header className="px-2 md:px-10 pt-8">
+      <div className="flex justify-between">
+        <span>What Did I Do This Month</span>
+        <div>
+          <span>Welcome Jovan</span>
+        </div>
+      </div>
+
+      <p className="text-center mt-2">Calendar</p>
+    </header>
+  );
+}
