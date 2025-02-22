094fcb3ec8217d121ac56e00bdd8bd9b41608fdf
Jovan <jovanmedford@gmail.com>
2025-02-22 08:50:35 -0500

    Add activity mock and types

---
+++ app/_types/types.ts
@@ -0,0 +1,27 @@
+export interface ActivityItem {
+  period: string;
+  activity: Activity;
+  progress: Progress;
+  totals: ActivityCount;
+}
+
+export interface Activity {
+  id: string;
+  name: string;
+  target: number;
+}
+
+export interface Progress {
+  successes: number[];
+  streaks?: {
+    longest: number;
+    current: number;
+  };
+}
+
+export interface ActivityCount {
+  completed: number;
+  remaining: number;
+}
+
+export interface Notes extends Record<string, string> {}
