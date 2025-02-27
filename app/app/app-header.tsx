1fd5903bf6ea6792c36d9ef6b46f50a0081044ee
Jovan <jovanmedford@gmail.com>
2025-02-26 21:39:56 -0500

    Protect app routes and fetch user name for app header

--- app/app/app-header.tsx
+++ app/app/app-header.tsx
@@ -1,10 +1,26 @@
+import { useQuery } from "@tanstack/react-query";
+import { fetchUserAttributes } from "aws-amplify/auth";
+
 export default function AppHeader() {
+  let { data, status } = useQuery({
+    queryKey: ["user"],
+    queryFn: fetchUserAttributes,
+  });
+
+  if (status == "error") {
+    return "...Error";
+  }
+
+  if (status == "pending") {
+    return <div className="w-full h-16"></div>
+  }
+
   return (
     <header className="px-2 md:px-10 pt-8">
       <div className="flex justify-between">
         <span>What Did I Do This Month</span>
         <div>
-          <span>Welcome Jovan</span>
+          <span>Hey {data?.given_name}</span>
         </div>
       </div>
 
--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -1,9 +1,54 @@
 "use client";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { getCurrentUser } from "aws-amplify/auth";
+import { Amplify } from "aws-amplify";
+import outputs from "@/amplify_outputs.json";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { showNotification } from "../_components/toast/toast";
+
+Amplify.configure(outputs);
 
 const queryClient = new QueryClient();
+
+const SessionProvider = ({ children }: { children: React.ReactNode }) => {
+  const router = useRouter();
+  const [status, setStatus] = useState<"pending" | "success">("pending");
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const user = await getCurrentUser();
+
+        if (!user) {
+          throw Error("Please log in to use the app.");
+        }
+
+        setStatus("success");
+      } catch (e) {
+        showNotification({
+          type: "error",
+          title: "An error occured",
+          description: e.message,
+        });
+        router.push("/login");
+      }
+    };
+
+    fetchUser();
+  }, []);
+
+  if (status == "pending") {
+    return "...loading";
+  }
+
+  return <>{children}</>;
+};
 
 export default function RootLayout({
   children,
@@ -13,7 +58,7 @@ export default function RootLayout({
   return (
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools />
-      {children}
+      <SessionProvider>{children}</SessionProvider>
     </QueryClientProvider>
   );
 }
