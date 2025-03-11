9e1bb877a343766bafc151b62439e72006a5a93e
Jovan <jovanmedford@gmail.com>
2025-03-11 19:46:20 -0400

    Add ability to update successes

--- app/app/calendar/activity-table.tsx
+++ app/app/calendar/activity-table.tsx
@@ -1,33 +1,95 @@
 "use client";
 
-import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { generateArray, getDbClient } from "@/app/utils";
 import { Filters, useFilterContext } from "./filter-context";
-import { QueryCommand } from "@aws-sdk/lib-dynamodb";
-import { AuthUser, getCurrentUser } from "aws-amplify/auth";
+import { QueryCommand, UpdateCommand } from "@aws-sdk/lib-dynamodb";
+import { getCurrentUser } from "aws-amplify/auth";
+import { showNotification } from "@/app/_components/toast/toast";
+import { Activity } from "@/app/_types/types";
 
-const DateSquares = ({
-  activityId = "activity",
-  successes = [],
+const addSuccess = async ({
+  activity,
+  successIndex,
 }: {
-  activityId: string;
-  successes: number[];
+  activity: Activity;
+  successIndex: number;
 }) => {
+  const docClient = getDbClient();
+
+  if (!docClient) {
+    console.log("Db client is not available");
+    return;
+  }
+
+  const command = new UpdateCommand({
+    TableName: "Activity-aqifu6gcxnc4hburjj2wfxjnwu-NONE",
+    Key: {
+      userId: activity.userId,
+      ["period#name"]: activity["period#name"],
+    },
+    UpdateExpression: "SET successes = list_append(:s, :i)",
+    ExpressionAttributeValues: {
+      ":s": activity.successes,
+      ":i": [successIndex],
+    },
+  });
+
+  const response = await docClient.send(command);
+
+  if (!response) {
+    throw Error();
+  }
+
+  return response.ItemCollectionMetrics?.ItemCollectionKey;
+};
+
+const DateSquares = ({ activity }: { activity: Activity }) => {
   let indices = generateArray(0, 31);
-  let successSet = new Set(successes);
+  const { filters } = useFilterContext();
+  let successSet = new Set(activity.successes);
+
+  const queryClient = useQueryClient();
+
+  let mutation = useMutation({
+    mutationFn: addSuccess,
+    onSuccess: () => {
+      return queryClient.invalidateQueries({
+        queryKey: ["activties", filters.year, filters.month, activity.userId],
+      });
+    },
+    onError: (e) => {
+      showNotification({
+        type: "error",
+        title: "Could not update your progress.",
+        description: "Please try again.",
+      });
+      console.log(e.message);
+    },
+  });
+
+  const handleSquareClicked = (e) => {
+    let successIndex = Number(e.target.dataset.index as string);
+    if (!successSet.has(successIndex)) {
+      mutation.mutate({ activity, successIndex });
+    }
+  };
+
   return (
     <div className="flex gap-4 w-40 m-auto md:w-full md:flex-wrap overflow-auto">
       {indices.map((index) => (
         <label
-          key={`${activityId}-${index}`}
+          key={`${activity}-${index}`}
           className="checkbox-parent flex justify-center w-6"
         >
           {index}
           <input
             readOnly
+            data-index={index}
+            onClick={handleSquareClicked}
             checked={successSet.has(index)}
             type="checkbox"
-            name={activityId}
+            name={activity["period#name"]}
           ></input>
         </label>
       ))}
@@ -43,7 +105,7 @@ const Row = ({ activity }: { activity: Activity }) => {
         <span className="text-xs font-normal">(0/{activity.target})</span>
       </th>
       <td className="w-6/12 md:w-8/12 overflow-x-auto">
-        <DateSquares activityId={activity.id} successes={activity.successes} />
+        <DateSquares activity={activity} />
       </td>
     </tr>
   );
@@ -91,7 +153,7 @@ export default function ActivityTable() {
   const { status, data, error } = useQuery({
     queryKey: ["activties", filters.year, filters.month, userData?.userId],
     queryFn: () => fetchActivities(filters, userId),
-    enabled: !!userId
+    enabled: !!userId,
   });
 
   if (status === "pending") {
@@ -107,7 +169,7 @@ export default function ActivityTable() {
       <tbody>
         {Array.isArray(data) && data.length > 0 ? (
           data.map((activity: Activity) => (
-            <Row key={`${activity.userId}-${activity["period#name"]}`} activity={activity}></Row>
+            <Row key={`${activity["period#name"]}`} activity={activity}></Row>
           ))
         ) : (
           <tr>
@@ -119,12 +181,4 @@ export default function ActivityTable() {
       </tbody>
     </table>
   );
-}
-
-interface Activity {
-  userId: string;
-  "period#name": string;
-  name: string;
-  target: number;
-  successes: number[];
 }
