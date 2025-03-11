843f56703f1843b62e69e876a1713725a1f6703f
Jovan <jovanmedford@gmail.com>
2025-03-11 19:42:10 -0400

    Get rid of outdated types

--- app/_types/types.ts
+++ app/_types/types.ts
@@ -1,28 +1,7 @@
-export interface ActivityItem {
-  period: string;
-  activity: Activity;
-  progress: Progress;
-  totals: ActivityCount;
-}
-
 export interface Activity {
   userId: string;
-  period: string;
+  "period#name": string;
   name: string;
   target: number;
-}
-
-export interface Progress {
   successes: number[];
-  streaks?: {
-    longest: number;
-    current: number;
-  };
 }
-
-export interface ActivityCount {
-  completed: number;
-  remaining: number;
-}
-
-export interface Notes extends Record<string, string> {}
