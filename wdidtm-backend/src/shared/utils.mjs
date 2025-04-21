a07942f10c24cd98cc75660d844df58348f9adc6
Jovan <jovanmedford@gmail.com>
2025-04-21 19:35:15 -0400

    Add slug and days uitlities

--- wdidtm-backend/src/shared/utils.mjs
+++ wdidtm-backend/src/shared/utils.mjs
@@ -11,3 +11,27 @@ export const createPostEvent = (email, body) => {
     body,
   };
 };
+
+// Taken from - https://stackoverflow.com/questions/1184334/get-number-days-in-a-specified-month-using-javascript
+export function daysInMonth(month, year) {
+  return new Date(year, month, 0).getDate();
+}
+
+export function daysLeft() {
+  let date = new Date();
+  let today = date.getDate();
+  return daysInMonth(date.getMonth() + 1, date.getFullYear()) - today + 1;
+}
+
+export function getSlug(sk) {
+  return sk.split("#")[0];
+}
+
+export function generateSlug(name) {
+  return name
+    .toString()
+    .toLowerCase()
+    .trim()
+    .replace(/[\s\W-]+/g, "-")
+    .replace(/^-+|-+$/g, "");
+}
