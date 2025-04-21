3ba68ffc16185585be1f0096d238ba4b552ff8ea
Jovan <jovanmedford@gmail.com>
2025-04-21 19:34:05 -0400

    Add error message constants

--- wdidtm-backend/src/shared/errors.mjs
+++ wdidtm-backend/src/shared/errors.mjs
@@ -1,4 +1,6 @@
 
+export const NOT_ENOUGH_DAYS_MESSAGE = "There aren't enough days left to meet this target."
+export const MISSING_PROPERTIES_MESSAGE = "missing required properties."
 
 export class BadInputError extends Error {
     name = "BadInputError"
