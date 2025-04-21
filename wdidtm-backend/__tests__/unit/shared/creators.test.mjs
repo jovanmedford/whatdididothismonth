7bff4e7d2f5d1ee4c63756a83fb5dbe975f6909a
Jovan <jovanmedford@gmail.com>
2025-04-21 19:36:13 -0400

    Add addLogFunction with minor refactoring to createActivity and createCategory

--- wdidtm-backend/__tests__/unit/shared/creators.test.mjs
+++ wdidtm-backend/__tests__/unit/shared/creators.test.mjs
@@ -1,7 +1,12 @@
 import { DynamoDBDocumentClient, PutCommand } from "@aws-sdk/lib-dynamodb";
-import { createCategory, createActivity } from "../../../src/shared/creators";
+import {
+  createCategory,
+  createActivity,
+  createLog,
+} from "../../../src/shared/creators";
 import { mockClient } from "aws-sdk-client-mock";
 import "aws-sdk-client-mock-jest";
+import { MISSING_PROPERTIES_MESSAGE, NOT_ENOUGH_DAYS_MESSAGE } from "../../../src/shared/errors.mjs";
 
 const ddbMock = mockClient(DynamoDBDocumentClient);
 let tableName = "Test-Table-123";
@@ -42,12 +47,15 @@ describe("create category", () => {
 });
 
 describe("create activity", () => {
+  beforeEach(() => {
+    ddbMock.reset();
+  });
+
   it("successfully creates an activity", () => {
     let input = {
       pk: "test@example.com",
       name: "Tennis",
       categoryId: "123",
-      description: "Favorite sport",
     };
 
     createActivity(input, ddbMock, tableName);
@@ -68,6 +76,82 @@ describe("create activity", () => {
 
     await expect(createActivity(category, ddbMock, tableName)).rejects.toThrow(
       "missing required properties"
+    );
+  });
+});
+
+describe("create log", () => {
+  let testEmail = "test@email.com";
+  beforeEach(() => {
+    ddbMock.reset();
+  });
+
+  it("successfully adds a new log", async () => {
+    let input = {
+      pk: testEmail,
+      year: 2025,
+      month: 4,
+      activityId: "tennis",
+      activityName: "Tennis",
+      categoryId: "health",
+      target: 10,
+      successes: []
+    };
+
+    await createLog(input, ddbMock, tableName);
+
+    expect(ddbMock).toHaveReceivedCommandWith(PutCommand, {
+      TableName: tableName,
+      Item: {
+        pk: testEmail,
+        sk: expect.stringContaining(
+          `LOG#${input.year}#${input.month}#${input.activityId}`
+        ),
+        ...input,
+      },
+      ConditionExpression: "attribute_not_exists(pk)",
+    });
+  });
+
+  it("throw error if activityId is missing", async () => {
+    let input = {
+      pk: testEmail,
+      year: 2025,
+      month: 4,
+      target: 10,
+    };
+
+    await expect(createLog(input, ddbMock, tableName)).rejects.toThrow(
+      MISSING_PROPERTIES_MESSAGE
+    );
+  });
+
+  it("throw error if target is missing", async () => {
+    let input = {
+      pk: testEmail,
+      year: 2025,
+      month: 4,
+      activityId: "tennis",
+      activityName: "Tennis",
+    };
+
+    await expect(createLog(input, ddbMock, tableName)).rejects.toThrow(
+      MISSING_PROPERTIES_MESSAGE
+    );
+  });
+
+  it("throw error if target is bigger than days remaining", async () => {
+    let input = {
+      pk: testEmail,
+      year: 2025,
+      month: 4,
+      target: 60,
+      activityId: "tennis",
+      activityName: "Tennis",
+    };
+
+    await expect(createLog(input, ddbMock, tableName)).rejects.toThrow(
+      NOT_ENOUGH_DAYS_MESSAGE
     );
   });
 });
--- wdidtm-backend/src/shared/creators.mjs
+++ wdidtm-backend/src/shared/creators.mjs
@@ -1,22 +1,16 @@
-import { PutCommand } from "@aws-sdk/lib-dynamodb";
-import { BadInputError } from "./errors.mjs";
-import { nanoid } from 'nanoid'
+import { PutCommand, TransactWriteCommand } from "@aws-sdk/lib-dynamodb";
+import {
+  BadInputError,
+  MISSING_PROPERTIES_MESSAGE,
+  NOT_ENOUGH_DAYS_MESSAGE,
+} from "./errors.mjs";
+import { daysLeft, generateSlug, getSlug } from "./utils.mjs";
 
 /**
  * Creates a new  category.
  */
 export const createCategory = async (input, dbClient, tableName) => {
-  // need to generate id
-
-  if (!input.name || !input.color || !input.icon) {
-    throw new BadInputError("You are missing required properties")
-  }
-
-  let category = {
-    ...input,
-    sk: `CATEGORY#${nanoid(8)}`
-  }
-
+  let category = createCategoryItem(input);
   let params = {
     TableName: tableName,
     Item: category,
@@ -27,31 +21,98 @@ export const createCategory = async (input, dbClient, tableName) => {
   return category;
 };
 
+export const createCategoryItem = (input) => {
+  if (!input.name || !input.color || !input.icon) {
+    throw new BadInputError("You are missing required properties");
+  }
+
+  let category = {
+    pk: input.pk,
+    sk: `CATEGORY#${generateSlug(input.name)}`,
+    color: input.color,
+    icon: input.icon,
+    name: input.name,
+  };
+
+  return category;
+};
+
 /**
  * Creates a new  activity.
  */
 export const createActivity = async (input, dbClient, tableName) => {
-  if (!input.name) {
-    throw new BadInputError("You are missing required properties")
-  }
-
-  let activity = {
-    ...input,
-    sk: `ACTIVITY#${nanoid(8)}`
-  }
-
+  let activity = createActivityItem(input);
   let params = {
     TableName: tableName,
     Item: activity,
-    ConditionExpression: "attribute_not_exists(pk)"
+    ConditionExpression: "attribute_not_exists(pk)",
+  };
+
+  await dbClient.send(new PutCommand(params));
+
+  return activity;
+};
+
+export const createActivityItem = (input) => {
+  if (!input.name) {
+    throw new BadInputError("You are missing required properties");
   }
 
-  await dbClient.send(new PutCommand(params))
+  let activity = {
+    pk: input.pk,
+    sk: `ACTIVITY#${generateSlug(input.name)}`,
+    name: input.name,
+  };
 
-  return activity
+  if (input.categoryId) activity.categoryId = input.categoryId;
+  return activity;
 };
 
 /**
  * Creates a new log for an activity for a given period.
  */
-export const createProgressItem = (period, activity, dbClient, tableName) => {};
+export const createLog = async (input, dbClient, tableName) => {
+  let log = createLogItem(input);
+
+  let params = {
+    TableName: tableName,
+    Item: log,
+    ConditionExpression: "attribute_not_exists(pk)",
+  };
+
+  await dbClient.send(new PutCommand(params));
+  return log;
+};
+
+export const createLogItem = (input) => {
+  if (!input || !input.activityId || !input.activityName || !input.target) {
+    throw new BadInputError(MISSING_PROPERTIES_MESSAGE);
+  }
+
+  if (input.target > daysLeft()) {
+    throw new BadInputError(NOT_ENOUGH_DAYS_MESSAGE);
+  }
+
+  let log = {
+    pk: input.pk,
+    sk: `LOG#${input.year}#${input.month}#${getSlug(input.activityId)}`,
+    year: input.year,
+    month: input.month,
+    target: input.target,
+    successes: [],
+    activityId: input.activityId,
+    activityName: input.activityName,
+  };
+
+  if (input.categoryId) log.categoryId = input.categoryId;
+
+  return log
+};
+
+export const runTransactions = async (transactItems, dbClient) => {
+  return await dbClient.send(
+    new TransactWriteCommand({
+      TransactItems: transactItems,
+    })
+  );
+};
