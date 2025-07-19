4a9eccafaed4c6c508e3f75668aa14a3156d6069
Jovan <jovanmedford@gmail.com>
2025-07-19 08:31:33 -0400

    Add get categories and get activities

--- wdidtm-backend/src/shared/getters.mjs
+++ wdidtm-backend/src/shared/getters.mjs
@@ -1,5 +1,6 @@
 import { GetCommand } from "@aws-sdk/lib-dynamodb";
-import { BadInputError } from "./errors.mjs";
+import { BadInputError, MISSING_PROPERTIES_MESSAGE } from "./errors.mjs";
+import { QueryCommand } from "@aws-sdk/client-dynamodb";
 
 export const getOneCategory = async (input, dbClient, tableName) => {
   if (!input.id || !input.pk) {
@@ -10,13 +11,13 @@ export const getOneCategory = async (input, dbClient, tableName) => {
     TableName: tableName,
     Key: {
       pk: input.pk,
-      sk: `CATEGORY#${input.id}`
-    }
+      sk: `CATEGORY#${input.id}`,
+    },
   };
 
   const data = await dbClient.send(new GetCommand(params));
 
-  console.log(data)
+  console.log(data);
   return data;
 };
 
@@ -29,11 +30,49 @@ export const getOneActivity = async (input, dbClient, tableName) => {
     TableName: tableName,
     Key: {
       pk: input.pk,
-      sk: `ACTIVITY#${input.activityId}`
-    }
+      sk: `ACTIVITY#${input.activityId}`,
+    },
   };
 
   const data = await dbClient.send(new GetCommand(params));
+
+  return data;
+};
+
+export const getAllCategories = async (input, dbClient, tableName) => {
+  if (!input.pk) {
+    throw new BadInputError(MISSING_PROPERTIES_MESSAGE);
+  }
+
+  let params = {
+    TableName: tableName,
+    KeyConditionExpression: "pk = :pk and begins_with(sk, :sk)",
+    ExpressionAttributeValue: {
+      ":pk": input.pk,
+      ":sk": "CATEGORY#",
+    },
+  };
+
+  const data = await dbClient.send(new QueryCommand(params));
+
+  return data;
+};
+
+export const getAllActivities = async (input, dbClient, tableName) => {
+  if (!input.pk) {
+    throw new BadInputError(MISSING_PROPERTIES_MESSAGE);
+  }
+
+  let params = {
+    TableName: tableName,
+    KeyConditionExpression: "pk = :pk and begins_with(sk, :sk)",
+    ExpressionAttributeValue: {
+      ":pk": input.pk,
+      ":sk": "ACTIVITY#",
+    },
+  };
+
+  const data = await dbClient.send(new QueryCommand(params));
 
   return data;
 };
