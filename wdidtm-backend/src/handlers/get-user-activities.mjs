9ac02c1552bc65c97b05126e0acfeb29d627d819
Jovan <jovanmedford@gmail.com>
2025-08-09 08:24:35 -0400

    Feat: Create postgres infrastructure and update getUserActivities lambda to match

--- wdidtm-backend/src/handlers/get-user-activities.mjs
+++ wdidtm-backend/src/handlers/get-user-activities.mjs
@@ -1,13 +1,20 @@
 // Create clients and set shared const values outside of the handler.
 
 // Create a DocumentClient that represents the query to add an item
-import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import { DynamoDBDocumentClient, QueryCommand } from "@aws-sdk/lib-dynamodb";
+import { Client } from "pg";
 
-const client = new DynamoDBClient({});
-const ddbDocClient = DynamoDBDocumentClient.from(client);
+const client = new Client({
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  host: process.env.DB_PROXY_ENDPOINT,
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: true,
+  }
+});
 
-const tableName = process.env.WDIDTM_TABLE;
+await client.connect();
 
 export const getUserActivitiesHandler = async (event) => {
   const corsHeaders = {
@@ -45,22 +52,30 @@ export const getUserActivitiesHandler = async (event) => {
   }
 
   const email = event.requestContext.authorizer.claims.email;
-
-  var params = {
-    TableName: tableName,
-    KeyConditionExpression: "pk = :pk and begins_with(#sk, :period)",
-    ExpressionAttributeNames: {
-      "#sk": "sk",
-    },
-    ExpressionAttributeValues: {
-      ":pk": `${email}`,
-      ":period": `PROGRESS#${year}#${month}`,
-    },
-  };
-
+  let res;
   try {
-    const data = await ddbDocClient.send(new QueryCommand(params));
-    var items = data.Items;
+    let text = `SELECT
+    activities.id AS "activityId",
+    activities.label AS "activityName",
+    categories.label AS "categoryName",
+    categories.color AS "categoryColor",
+    categories.icon AS "categoryIcon",
+    json_agg (success_logs.day) successes,
+    activity_logs.target target
+FROM
+    users
+    JOIN categories ON users.id = categories.user_ID
+    JOIN activities ON categories.id = activities.cat_id
+    JOIN activity_logs ON activities.id = activity_logs.activity_id
+    JOIN success_logs ON activity_logs.id = success_logs.activity_log_id
+WHERE
+    users.email = $1 AND activity_logs.year = $2 AND activity_logs.month = $3;
+GROUP BY
+    categories.label,
+    activities.label,
+    activity_logs.year,
+    activity_logs.month;`;
+    res = await client.query(text, [email, year, month]);
   } catch (err) {
     console.log("Console logged error --->", err);
     return {
@@ -73,7 +88,7 @@ export const getUserActivitiesHandler = async (event) => {
   const response = {
     statusCode: 200,
     headers: corsHeaders,
-    body: JSON.stringify(items),
+    body: res,
   };
 
   // All log statements are written to CloudWatch
