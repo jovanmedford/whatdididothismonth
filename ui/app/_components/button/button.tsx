56c97abdab58b8db8e3387ac4b8bc04b9363f891
Jovan <jovanmedford@gmail.com>
2025-09-18 08:44:45 -0400

    Feat: add button sizes and remove background on default buttons

--- ui/app/_components/button/button.tsx
+++ ui/app/_components/button/button.tsx
@@ -1,16 +1,21 @@
 import { ButtonHTMLAttributes } from "react";
-import "./button.css"
-
+import "./button.css";
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-    variant?: "default" | "emphasized"
+  variant?: "default" | "emphasized";
+  size?: "small" | "medium" | "large";
 }
 
-export default function Button({ variant = "default", children, ...props}: ButtonProps) {
+export default function Button({
+  variant = "default",
+  size = "medium",
+  children,
+  ...props
+}: ButtonProps) {
   return (
     <button
       {...props}
-      className={props.className + ` btn btn-${variant} ` + "py-2 px-4"}
+      className={props.className + ` btn btn-${variant} btn-${size}`}
     >
       {children}
     </button>
