bb78e6250134799e644bdc425b487f54ee216775
Jovan <jovanmedford@gmail.com>
2025-09-18 08:45:58 -0400

    Feat: add logout button

--- ui/app/app/app-header.tsx
+++ ui/app/app/app-header.tsx
@@ -1,15 +1,29 @@
-import { useQuery } from "@tanstack/react-query";
-import { fetchUserAttributes, getCurrentUser } from "aws-amplify/auth";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { fetchUserAttributes, signOut } from "aws-amplify/auth";
+import Button from "../_components/button/button";
+import { showNotification } from "../_components/toast/toast";
+import { useRouter } from "next/navigation";
 
 export default function AppHeader() {
+  const router = useRouter();
+  const queryClient = useQueryClient();
+  const mutation = useMutation({
+    mutationFn: async () => await signOut(),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["session"] });
+      router.push("/");
+    },
+    onError: () =>
+      showNotification({
+        title: "Error",
+        type: "error",
+        description: "Error logging out.",
+      }),
+  });
+
   let { data, status } = useQuery({
     queryKey: ["user"],
     queryFn: fetchUserAttributes,
-  });
-
-  useQuery({
-    queryKey: ["currentUser"],
-    queryFn: getCurrentUser,
   });
 
   if (status == "error") {
@@ -20,12 +34,19 @@ export default function AppHeader() {
     return <div className="w-full h-16"></div>;
   }
 
+  const handleLogout = async () => {
+    mutation.mutate();
+  };
+
   return (
     <header className="px-2 md:px-10 pt-8">
       <div className="flex justify-between">
         <span>What Did I Do This Month</span>
-        <div>
-          <span>Hey {data?.given_name}</span>
+        <div className="flex flex-col">
+          <span>Hey {data?.given_name || "friend"}</span>
+          <Button size="small" onClick={handleLogout}>
+            Logout
+          </Button>
         </div>
       </div>
 
