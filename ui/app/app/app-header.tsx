c58021618f8d6f741e5f025b418c48ed64334c42
Jovan <jovanmedford@gmail.com>
2025-09-30 13:29:15 -0400

    Feat: add logo to app header

--- ui/app/app/app-header.tsx
+++ ui/app/app/app-header.tsx
@@ -3,6 +3,7 @@ import { fetchUserAttributes, signOut } from "aws-amplify/auth";
 import Button from "../_components/button/button";
 import { showNotification } from "../_components/toast/toast";
 import { useRouter } from "next/navigation";
+import Logo from "../_components/logo/logo";
 
 export default function AppHeader() {
   const router = useRouter();
@@ -41,10 +42,10 @@ export default function AppHeader() {
   };
 
   return (
-    <header className="px-2 md:px-10 pt-8">
-      <div className="flex justify-between">
-        <span>What Did I Do This Month</span>
-        <div className="flex flex-col">
+    <header className="px-2 md:px-10 pt-4">
+      <div className="flex justify-between items-center">
+        <Logo className="w-20 md:w-60" />
+        <div className="flex items-center gap-x-2">
           <span>Hey {data?.given_name || "friend"}</span>
           <Button size="small" onClick={handleLogout}>
             Logout
