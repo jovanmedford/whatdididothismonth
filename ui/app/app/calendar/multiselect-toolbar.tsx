27a5797da02a5d0b1e45c437337f30d03a0e894d
Jovan <jovanmedford@gmail.com>
2025-09-30 13:28:27 -0400

    Feat: add multiselect toolbar component

---
+++ ui/app/app/calendar/multiselect-toolbar.tsx
@@ -0,0 +1,20 @@
+import { useTableStore } from "./use-table";
+import Button from "@/app/_components/button/button";
+import { Trash } from "lucide-react";
+
+export default function MultiselectToolbar() {
+  const selected = useTableStore((state) => state.selected);
+
+  if (selected.length < 1) return <div className="h-9 w-4"></div>;
+
+  return (
+    <div
+      className={`
+        fixed top-0 left-0 right-0 flex mb-1 px-4 py-5 justify-center
+        md:static md:ml-4 md:rounded-xl md:px-3 md:py-1 bg-gray-200  items-center`}
+    >
+      <span className="mr-2">{selected.length} selected</span>
+      <Button icon={Trash} size="small" className="rounded-4xl"></Button>
+    </div>
+  );
+}
